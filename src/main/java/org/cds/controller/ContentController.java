33a528e02a44bb9602aa3c0c267d76c184561e00
njk nkl <baev-lev1@mail.ru>
2022-07-14 12:56:57 +0700

    Add tests

--- src/main/java/org/cds/controller/ContentController.java
+++ src/main/java/org/cds/controller/ContentController.java
@@ -3,6 +3,7 @@ package org.cds.controller;
 import org.cds.model.web.WebContent;
 import org.cds.service.web.WebContentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -19,9 +21,10 @@ public class ContentController {
     private WebContentService webContentService;
 
     @RequestMapping(path = "/content/get/{content-id}", method = RequestMethod.GET)
-    private WebContent getContentById(@PathVariable("content-id") UUID id) {
-
-        return webContentService.findContentById(id);
+    private ResponseEntity<WebContent> getContentById(@PathVariable("content-id") UUID id) {
+        WebContent webContent = webContentService.findContentById(id);
+        if (webContent.getData()==null) return ResponseEntity.notFound().build();
+        return ResponseEntity.of(Optional.of(webContent));
     }
     @PostMapping ("/content/save")
     private WebContent saveContent(@RequestBody String data){
--- src/test/java/TestCDS.java
+++ src/test/java/TestCDS.java
@@ -12,6 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,8 +32,14 @@ public class TestCDS {
 
 
     @Test
-    void targetSearchTest() throws Exception {
+    void targetTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/target/get"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+    @Test
+    void contentTest() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/content/get"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
@@ -40,13 +47,44 @@ public class TestCDS {
     void contentSearchTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/content/get/af3cfff8-c9ed-411a-aa58-5c3247a2a5a4"))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("{\"guid\":\"af3cfff8-c9ed-411a-aa58-5c3247a2a5a4\",\"data\":\"{\\\"header\\\": \\\"thom22\\\", \\\"backgroundImg\\\": \\\"qweqwe.img\\\",\\\"text\\\": \\\"kglfgkflg\\\"}\"}"));
     }
     @Test
-    void viewedSearchTest() throws Exception {
+    void contentSearch404Test() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.get("/content/get/af3cfff8-c9ed-411a-aa58-5c3247a2a5a5"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void viewedTest() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get("/viewed/get"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
+    @Test
+    void viewedSaveTest() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.post("/viewed/save")
+                        .content("{\"contentId\": \"46f85973-64ed-4aa3-9164-e455448b9028\", \"userId\": \"ccde47e7-7139-400f-8a65-6320d9156f68\"}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+    }
+    @Test
+    void contentSaveTest() throws Exception{
+        mvc.perform(MockMvcRequestBuilders.post("/content/save")
+                .content("{ \"data\": {\n" +
+                        "    \"header\": \"example\",\n" +
+                        "    \"backgroundImg\": \"img.img\",\n" +
+                        "    \"text\": \"cat\"\n" +
+                        "  },\n" +
+                        "  \"guid\": \"d0844183-bc7f-4594-a225-3d1579295d60\"\n" +
+                        "}")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
 
 }
